c96d7afca701c8452be726fdcc8cdd24c2defcbe
Dattatreya S Vellal <41127409+dsvellal@users.noreply.github.com>
2022-09-23 11:37:12 -0400

    Update CaseNormalizerInputProcessorTest.java

--- Anagram/AnagramProgram/tst/com/example/anagram/processors/CaseNormalizerInputProcessorTest.java
+++ Anagram/AnagramProgram/tst/com/example/anagram/processors/CaseNormalizerInputProcessorTest.java
@@ -1,11 +1,4 @@
-/*
- * Copyright of Philips, 2019
- *
- * This file is subject to the terms and conditions defined by Philips,
- * visit https://www.ip.philips.com/licensing/ for more details.
- *
- */
-package com.philips.anagram.processors;
+package com.example.anagram.processors;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -16,7 +9,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.philips.anagram.constants.ProgramBehaviorKeys;
+import com.example.anagram.constants.ProgramBehaviorKeys;
 
 public class CaseNormalizerInputProcessorTest {
 
