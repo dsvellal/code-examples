5a155823f1e9f4642c95807384277899aa4fda19
Dattatreya S Vellal <41127409+dsvellal@users.noreply.github.com>
2022-09-23 11:34:10 -0400

    Update TestClass.java

--- Anagram/AnagramFunctionalTests/src/com/example/anagram/functional/tests/TestClass.java
+++ Anagram/AnagramFunctionalTests/src/com/example/anagram/functional/tests/TestClass.java
@@ -1,19 +1,13 @@
-/*
- * Copyright of Philips, 2019
- *
- * This file is subject to the terms and conditions defined by Philips,
- * visit https://www.ip.philips.com/licensing/ for more details.
- *
- */
-package com.philips.anagram.functional.tests;
+
+package com.example.anagram.functional.tests;
 
 import java.util.List;
 import java.util.Map;
 
-import com.philips.anagram.constants.ProgramBehaviorKeys;
-import com.philips.anagram.main.MainThread;
-import com.philips.anagram.utils.FileUtility;
-import com.philips.anagram.utils.ProgramBehaviorReaderUtility;
+import com.example.anagram.constants.ProgramBehaviorKeys;
+import com.example.anagram.main.MainThread;
+import com.example.anagram.utils.FileUtility;
+import com.example.anagram.utils.ProgramBehaviorReaderUtility;
 
 /**
  *
